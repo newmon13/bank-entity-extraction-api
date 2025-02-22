528b816c53559ac1e91ee8d0ffe5a5eb8060cca2
newmo13 <kubalipka123@gmail.com>
2025-02-22 01:21:15 +0100

    Initial commit

---
+++ src/main/java/dev/jlipka/bankentityextractionapi/BankEntityExtractionApiApplication.java
@@ -0,0 +1,13 @@
+package dev.jlipka.bankentityextractionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankEntityExtractionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankEntityExtractionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jlipka/bankentityextractionapi/BankEntityExtractionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jlipka.bankentityextractionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEntityExtractionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
