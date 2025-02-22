daf545fbb1743f6a8f0ebeacf9f3f47f2ee8ca25
newmo13 <kubalipka123@gmail.com>
2025-02-22 01:23:41 +0100

    updated README.md

--- src/main/java/dev/jlipka/bankentityextractionapi/BankEntityExtractionApiApplication.java
+++ src/main/java/dev/jlipka/bankentityextractionapi/BankEntityExtractionApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankEntityExtractionApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BankEntityExtractionApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BankEntityExtractionApiApplication.class, args);
+    }
 
 }
--- src/test/java/dev/jlipka/bankentityextractionapi/BankEntityExtractionApiApplicationTests.java
+++ src/test/java/dev/jlipka/bankentityextractionapi/BankEntityExtractionApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BankEntityExtractionApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
